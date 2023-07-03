87b7508f3db8e5cb009b6ca3f36617d4eb7bbf2f
pyromechanical <andreaogara@gmail.com>
2023-07-03 12:25:23 -0400

    lifted types from parse.rs, added BoardState::new

---
+++ src/gamestate.rs
@@ -0,0 +1,68 @@
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct ParsedGameState {
+    pub piece_position: [[char; 8]; 8],
+    pub active_player: Player,
+    pub castling_rights: CastlingRights,
+    pub en_passant_target: Option<(File, Rank)>,
+    pub half_turn_clock: u8,
+    pub full_turn_clock: u32,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Player {
+    Black,
+    White,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct CastlingRights {
+    pub black_kingside: bool,
+    pub black_queenside: bool,
+    pub white_kingside: bool,
+    pub white_queenside: bool,
+}
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[repr(u8)]
+pub enum File {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+    E = 4,
+    F = 5,
+    G = 6,
+    H = 7,
+}
+
+impl TryFrom<char> for File {
+    type Error = char;
+    fn try_from(file: char) -> Result<File, Self::Error> {
+        match file {
+            'a'..='h' => Ok(unsafe { std::mem::transmute((file as u8) - b'a') }),
+            x => Err(x),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[repr(u8)]
+pub enum Rank {
+    One = 0,
+    Two = 1,
+    Three = 2,
+    Four = 3,
+    Five = 4,
+    Six = 5,
+    Seven = 6,
+    Eight = 7,
+}
+
+impl TryFrom<char> for Rank {
+    type Error = char;
+    fn try_from(rank: char) -> Result<Rank, Self::Error> {
+        match rank {
+            '1'..='8' => Ok(unsafe { std::mem::transmute((rank as u8) - b'1') }),
+            x => Err(x),
+        }
+    }
+}
