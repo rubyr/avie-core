32d7e0413cfa8b68ecc14c76d274235e5688e671
pyromechanical <andreaogara@gmail.com>
2023-07-03 12:25:23 -0400

    lifted types from parse.rs, added BoardState::new

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
-
+use crate::gamestate::{ParsedGameState, Player, File, Rank};
 #[cfg(test)]
 mod test {}
 
@@ -10,17 +10,21 @@ struct PlayerState {
     rooks: u64,
     pawns: u64,
     king_castle: bool,
-    queen_castle: bool
+    queen_castle: bool,
 }
-
 
 impl PlayerState {
     ///function uses a quirk of binary representation to verify that there are no duplicate pieces on the board.
-    /// when no bits are shared between two numbers, addition gives the same result as binary OR. by adding all 
+    /// when no bits are shared between two numbers, addition gives the same result as binary OR. by adding all
     /// bitboards together and comparing with all bitboards OR'd together, we can ensure that a player's state is valid.
     fn is_valid(&self) -> bool {
         let mut valid = self.king.count_ones() == 1;
-        let add_board: u128 = self.king as u128 + self.queens as u128 + self.bishops as u128 + self.knights as u128 + self.rooks as u128 + self.pawns as u128;
+        let add_board: u128 = self.king as u128
+            + self.queens as u128
+            + self.bishops as u128
+            + self.knights as u128
+            + self.rooks as u128
+            + self.pawns as u128;
         valid &= add_board == self.all_pieces() as u128;
         valid
     }
@@ -28,12 +32,6 @@ impl PlayerState {
         self.king | self.queens | self.bishops | self.knights | self.rooks | self.pawns
     }
 }
-
-enum Player {
-    Black,
-    White
-}
-
 
 ///En Passant Target representation:
 ///  0bX0000000: active flag. if 1, there was no en passant on the previous turn, and all other bits are ignored.
@@ -47,7 +45,7 @@ impl EnPassantTarget {
             0 => Some(Player::White),
             1 => Some(Player::Black),
             2 | 3 => None,
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
     fn targeted_square(&self) -> u64 {
@@ -61,10 +59,69 @@ pub struct BoardState {
     en_passant_target: EnPassantTarget,
     active_player: Player,
     half_counter: u8,
-    full_counter: u32 //pretty sure it's impossible to have a chess game go longer than 4 billion moves, but we'll see
+    full_counter: u32, //pretty sure it's impossible to have a chess game go longer than 4 billion moves, but we'll see
+}
+
+fn into_bitboard(matched_char: char, board: &[[char; 8]; 8]) -> u64 {
+    let board: &[char; 64] = unsafe { std::mem::transmute(board) };
+    let bool_board = board.map(|x| x == matched_char);
+    bool_board
+        .iter()
+        .fold(0u64, |result, &x| (result << 1) ^ x as u64)
+}
+
+fn player_from_gamestate(
+    player: Player,
+    gamestate: &ParsedGameState
+) -> PlayerState {
+    let board = &gamestate.piece_position;
+    let (chars, king_castle, queen_castle) = if player == Player::Black {
+        (
+            ['r', 'b', 'n', 'q', 'k', 'p'],
+            gamestate.castling_rights.black_kingside,
+            gamestate.castling_rights.black_queenside,
+        )
+    } else {
+        (
+            ['R', 'B', 'N', 'Q', 'K', 'P'],
+            gamestate.castling_rights.white_kingside,
+            gamestate.castling_rights.white_queenside,
+        )
+    };
+    let rooks = into_bitboard(chars[0], board);
+    let bishops = into_bitboard(chars[1], board);
+    let knights = into_bitboard(chars[2], board);
+    let queens = into_bitboard(chars[3], board);
+    let king = into_bitboard(chars[4], board);
+    let pawns = into_bitboard(chars[5], board);
+    PlayerState{rooks, bishops, knights, queens, king, pawns, king_castle, queen_castle}
+}
+
+fn en_passant_target(target: &Option<(File, Rank)>) -> EnPassantTarget {
+    match target {
+        None => EnPassantTarget(0x80),
+        Some((file, rank)) => {
+            let playermask= if *rank == Rank::Six {0b01000000u8} else {0u8};
+            let square: u8 = (7 - *file as u8) + (*rank as u8 * 8);
+            EnPassantTarget(playermask | square)
+        }
+    }
 }
 
 impl BoardState {
+    fn new(game: ParsedGameState) -> Self {
+        let black = player_from_gamestate(Player::Black, &game);
+        let white = player_from_gamestate(Player::White, &game);
+        let en_passant_target = en_passant_target(&game.en_passant_target);
+        let result = Self{black, white, en_passant_target, active_player: game.active_player, half_counter: game.half_turn_clock, full_counter: game.full_turn_clock};
+        debug_assert_eq!(result.is_valid(), true);
+        result 
+    }
+
+    fn is_valid(&self) -> bool{
+        self.white.is_valid() & self.black.is_valid()
+    }
+
     fn all_pieces(&self) -> u64 {
         self.black.all_pieces() | self.white.all_pieces()
     }
