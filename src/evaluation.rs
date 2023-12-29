97dda15292835e6ba84c39a49d48a5294b149530
pyromechanical <andreaogara@gmail.com>
2023-12-28 20:53:51 -0500

    removed sorting

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -159,7 +159,7 @@ fn search(
         }
         return 0; //draw
     }
-    sort_moves(board, moves);
+    //sort_moves(board, moves);
     for mov in moves {
         *nodes += 1;
         board.make_move(*mov);
@@ -192,7 +192,7 @@ pub fn choose_best_move(
     let mut nodes = 0;
     let mut depth = 0;
     let mut best_score = WORST_SCORE;
-    sort_moves(board, moves);
+    //sort_moves(board, moves);
     while !should_stop.load(Ordering::Relaxed) {
         depth += 1;
         for i in 0..moves.len() {
